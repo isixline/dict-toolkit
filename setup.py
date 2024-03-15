671beb3b96e5915b41ebc8f9af5ed9f28e65ed8c
isixline <isixline@qq.com>
2024-03-15 13:03:03 +0800

    refactor: move to dict-toolkit

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='e-dict',
+    name='dict-toolkit',
     version='0.0.1',
     packages=find_packages(),
+    package_dir={'': 'dict-toolkit'}, 
+    package_data={
+        'e-dict': ['dict/*.csv'],
+    },
+    include_package_data=True,
     install_requires=[],
 )
+
