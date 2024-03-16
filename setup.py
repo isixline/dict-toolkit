3f9c3bc95ce7630c7f18a3cdb75a35569882a444
isixline <isixline@qq.com>
2024-03-16 10:11:06 +0800

    chore: update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dict-toolkit',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     package_data={
         'dict_toolkit': ['data/dict/split/*.csv'],
