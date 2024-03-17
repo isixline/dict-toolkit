9b6398e386c623a499f1c8620f90edd894984f2b
isixline <isixline@qq.com>
2024-03-17 15:37:10 +0800

    docs: update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='dict-toolkit',
-    version='0.0.4',
+    version='0.0.5',
+    long_description=open('README.md').read(),
     packages=find_packages(),
     package_data={
         'dict_toolkit': ['data/dict/split/*.csv'],
